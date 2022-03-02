fe45374f7ee0895234d74e1b7947b4da12a032e1
Cristian <cristiandescosido@gmail.com>
2022-03-02 11:22:01 -0300

    Ruteo de los elementos de imagen

--- src/components/Thumb/index.js
+++ src/components/Thumb/index.js
@@ -1,12 +1,20 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 
 // Styles
 import { Image } from "./Thumb.styles";
 
 
-const Thumb = ({image, movieId, clickable}) => (
+const Thumb = ({ image, movieId, clickable }) => (
     <div>
-        <Image src={image} alt='Movie-thumb'/>
+        {clickable ? (
+            <Link to={`/${movieId}`}>
+                <Image src={image} alt='Movie-thumb' />
+            </Link>
+        ) : (
+            <Image src={image} alt='Movie-thumb' />
+        )
+        }
     </div>
 );
 
