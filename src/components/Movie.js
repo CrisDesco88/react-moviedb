bcf6b6e056c2e642ad93d22e0c64279e720b8ab5
Cristian <cristiandescosido@gmail.com>
2022-03-03 11:56:10 -0300

    Creacion de Bread Crump para pelicula indivudual

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -5,6 +5,7 @@ import { Params, useParams } from 'react-router-dom';
 import { IMAGE_BASE_URL, POSTER_SIZE } from '../config';
 
 //Components
+import BreadCrumb from './BreadCrumb';
 import Grid from './Grid';
 import Spinner from './Spinner';
 
@@ -19,11 +20,14 @@ const Movie = () => {
 
     const {state: movie, loading, error} = useMovieFetch(movieId);
 
+    if(loading) return <Spinner/>;
+    if(error) return <div>Something went wrong...</div>
+
     console.log(movie); 
 
     return (
     <>
-        <div>Movie</div>
+        <BreadCrumb movieTitle={movie.original_title}/>
     </>
     );}
 
