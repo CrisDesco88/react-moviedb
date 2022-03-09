6e89f0e77964c0e06c5bf3ef511c865045217440
Cristian <cristiandescosido@gmail.com>
2022-03-09 10:15:17 -0300

    Estilizado Movie Info Bar

--- src/components/MovieInfoBar/MovieInfoBar.styles.js
+++ src/components/MovieInfoBar/MovieInfoBar.styles.js
@@ -1,6 +1,43 @@
 import styled from "styled-components";
 
-export const Wrapper = styled.div``;
+export const Wrapper = styled.div`
+    display: flex;
+    align-items: center;
+    min-height: 100px;
+    background: var(--darkGrey);
+    padding: 0 20px;
+`;
 
-export const Content = styled.div``;
+export const Content = styled.div`
+    display: flex;
+    justify-content: space-around;
+    width: 100%;
+
+    .column {
+        display: flex;
+        background: var(--medGrey);
+        border-radius: 20px;
+        padding: 10px 20px;
+
+        @media screen and (max-width:768px){
+            margin-top: 20px;
+            width: 50%;
+            min-width: 179px;
+            justify-content: center;
+        }
+
+        p {
+            margin: 0;
+        }
+    }
+
+    @media screen and (max-width: 768px) {
+        flex-direction: column;
+        align-items: center;
+        
+    }
+
+`;
+
+
 
